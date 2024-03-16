8b6c9ab45bebc848dae4e01531c1e64fc7395aff
ZhadaevAlex <zhadaevs@yandex.ru>
2024-03-16 14:40:18 +0300

    Added @SafeVarargs

--- src/com/gridnine/testing/FlightFilter.java
+++ src/com/gridnine/testing/FlightFilter.java
@@ -1,14 +1,14 @@
 package com.gridnine.testing;
 
 import com.gridnine.testing.entity.Flight;
-import com.gridnine.testing.utils.RulesBuilder;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class FlightFilter {
-    public List<Flight> filter(List<Flight> flights, Predicate<Flight>... rules) {
+    @SafeVarargs
+    public final List<Flight> filter(List<Flight> flights, Predicate<Flight>... rules) {
         List<Flight> result = new LinkedList<>();
 
         for (Flight flight : flights) {
